ff637b36b8af4fe8e9e09b2c7389803903e43e6d
Quartzs_Book <progquartz@gmail.com>
2022-08-12 22:07:50 +0900

    카메라 무빙 수정 / 맵 세팅 - 기차 씬 변경에 따른 맵 설정 변경 - 카메라가 기차 위치를 따라가고, 맵을 볼 수 있는 위치로 수정.

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -5,22 +5,15 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     [SerializeField]
-    GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //player = GameObject.Find("player_test(Clone)");
-    }
+    private GameObject Train;
 
+    private void Start()
+    {
+        Train = Train.transform.GetChild(0).gameObject;
+    }
     // Update is called once per frame
     void Update()
     {
-        if (player == null)
-        {
-            GameObject playerObj = GameObject.Find("player_test(Clone)");
-            if (playerObj.GetComponents<MyPlayer>() != null)
-                player = playerObj;
-        }
-        transform.position = player.transform.position + new Vector3(0, 14f, -8f);
+        transform.position = new Vector3 (Train.transform.position.x - 5.0f, transform.position.y, transform.position.z);
     }
 }
