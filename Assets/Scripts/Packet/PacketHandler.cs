6febb7e997751773549c4a55219ab20996ccf6ff
BaekMinSu <37802354+SulRang@users.noreply.github.com>
2022-09-08 11:16:03 +0900

    delete mapseedmanager

--- Assets/Scripts/Packet/PacketHandler.cs
+++ Assets/Scripts/Packet/PacketHandler.cs
@@ -61,7 +61,7 @@ class PacketHandler
 		S_BroadcastMapSeed pkt = packet as S_BroadcastMapSeed;
 		ServerSession serverSession = session as ServerSession;
 
-		MapSeedManager.Instance.UpdateSeed(pkt.mapSeed);
+		//MapSeedManager.Instance.UpdateSeed(pkt.mapSeed);
 	}
 	public static void S_BroadcastEnterRoomHandler(PacketSession session, IPacket packet)
 	{
